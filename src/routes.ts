34eab0d414ec78ce1c71bec276d61c1ad91d8f1c
Rizqy Nugroho <rizqy.nugroho@ist.id>
2023-03-31 23:29:18 +0700

    feat: rsvp

--- src/routes.ts
+++ src/routes.ts
@@ -3,10 +3,12 @@ import { Router } from "express"
 // Services
 import { router as AccountServices } from "@services/account"
 import { router as ProductServices } from "@services/product"
+import { router as RsvpServices } from "@services/rsvp"
 
 const router = Router()
 
 router.use("/auth", AccountServices)
 router.use("/product", ProductServices)
+router.use("/rsvp", RsvpServices)
 
 export { router as GlobalRouter }
