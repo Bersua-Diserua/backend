419b1b59b08709afb7db9656533f722313e361ed
Rizqy Nugroho <rizqy.nugroho@ist.id>
2023-04-08 02:47:52 +0700

    fix: cleanup

--- src/packages/utils/date.ts
+++ src/packages/utils/date.ts
@@ -1,5 +1,12 @@
 export function getDateWithoutTime(date: Date | string) {
+  console.log({
+    before: date,
+  })
+
   const parsed = new Date(date)
+  console.log({
+    after: parsed.toISOString(),
+  })
 
   return {
     day: parsed.getDate(),
