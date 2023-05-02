99baca25cbffec0296c283ae37b3f73e4e2bbf43
Rizqy Prastya Ari Nugroho <54632504+rizqyn9@users.noreply.github.com>
2023-05-02 20:27:24 +0700

    chore: whatsapp response

--- src/services/bot/controller/response.ts
+++ src/services/bot/controller/response.ts
@@ -1,11 +1,16 @@
 export async function getDefaultMessage() {
-  return `Halo Kawula Sérua, Namaku SEVA ( Sérua Virtual Assist ) Ada yang bisa SEVA bantu?
+  return `Halo Kawula Sérua, Namaku SEVA (Sérua Virtual Assist) Ada yang bisa SEVA bantu?
 1. Sérua Menu
 2. Reservasi Dine In
 3. Promo
 4. Sérua Merchandise
 5. Sérua Live Chat
-
+  
 *) balas dengan angka 1/2/3/4/5
-dapatkan promo menarik setiap transaksi di Sérua *S&K Berlaku`
+  
+Sérua Operational Hours : 
+Monday - Saturday 08.00 - 23.00
+Sunday 07.00 - 22.00
+   
+Dapatkan promo menarik setiap transaksi di Sérua *S&K Berlaku`
 }
