5f9c46d1ee0b66d9faafb85ba715f74dd7d3bece
Rei Yan <hariyantoriyan.hr@gmail.com>
2023-05-13 23:32:50 +0700

    chore: debug

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import "./polyfill"
 import "@packages/config"
 import "@packages/rabbitmq"
-import "@packages/csv"
+// import "@packages/csv"
 
 import { mongoConnect } from "./packages/mongodb"
 import { runServer } from "./server"
